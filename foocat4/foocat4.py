a7300b3aef860be16b5efb384f04fce14086b6de
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-19 16:31:59 -0800

    fixed style issue

--- foocat4/foocat4.py
+++ foocat4/foocat4.py
@@ -29,3 +29,4 @@ def catbind(a, b):
     concatenated = pd.concat([pd.Series(a.astype("str")),
                               pd.Series(b.astype("str"))])
     return pd.Categorical(concatenated)
+    
